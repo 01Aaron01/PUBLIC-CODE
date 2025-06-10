9138d453cc89b1022d80d3d3cce092474c346415
o1Aaron01 <aaron.abrams@cantab.net>
2025-06-09 17:05:09 -0700

    Update defi_fund_dashboard.py
    Error handling for anonymous

--- defi_fund_dashboard.py
+++ defi_fund_dashboard.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import time
 from datetime import datetime, timedelta
+import inspect
 
 # Page configuration
 st.set_page_config(
